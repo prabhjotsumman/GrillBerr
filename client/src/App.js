034a8b017186e132f4024be5221ae77bbd628749
prabhnith <prabhjot.nith@gmail.com>
2020-05-02 12:22:06 +0530

    Firebase SDK code added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 // import SignInSide from "./Components/login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import HomePage from "./Components/Homepage";
+import Homepage from "./Components/Homepage";
 import SignInSide from "./Components/login";
 import SignUpSide from "./Components/register";
 
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/signin" component={SignInSide} />
-      <Route path="/signup" component={SignUpSide} />
-    </Router>
-  );
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase/firebaseconfig";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  emailAuthProvider: new firebase.auth.EmailAuthProvider()
+};
+
+class App extends Component {
+  render() {
+    // const { user, signOut, signInWithGoogle } = this.props;
+
+    return (
+      <Router>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/signin" component={SignInSide } />
+        <Route path="/signup" component={SignUpSide } />
+      </Router>
+    );
+  }
 }
 
 export default App;
+// export default withFirebaseAuth({
+//   providers,
+//   firebaseAppAuth,
+// })(App);
---
+++ client/src/firebase/firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBay1FKM38K87X83hjz96mKpSfMq9W2KkA",
+  authDomain: "grillber-react.firebaseapp.com",
+  databaseURL: "https://grillber-react.firebaseio.com",
+  projectId: "grillber-react",
+  storageBucket: "grillber-react.appspot.com",
+  messagingSenderId: "106554603234",
+  appId: "1:106554603234:web:8d7b55465686e84ab090bb",
+  measurementId: "G-64EVYE61F8",
+};
+
+export default firebaseConfig;
