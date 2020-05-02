34131210f74a90576419ac15d74457971daef742
prabhnith <prabhjot.nith@gmail.com>
2020-05-02 12:58:40 +0530

    Navbar added

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
 
 class Homepage extends Component{
     render(){
         return (
             <div>
+                <Navbar/>
                 <h1> Grillber - Book Your BBQ now!</h1>
                 {/* <Link to="/signin">Signin</Link>
                 <Link to="/signup">Signup</Link> */}
