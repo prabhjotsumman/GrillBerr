901fc9b42fc7d29520b4f02132c99c1aad475526
prabhnith <prabhjot.nith@gmail.com>
2020-05-02 17:39:59 +0530

    store: authAction, authReducer barebone added

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
+import { LOGIN_FAILED, LOGIN_SUCCESS } from "../../constants/constants";
+
 const initState = {};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      console.log("login success");
+      return {
+        ...state,
+        authError: null,
+      };
+
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        authError: "LOGIN FAILED",
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
