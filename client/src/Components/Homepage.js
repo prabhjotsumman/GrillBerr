1ce7ade9b96a0d4cfc78d5ba81e62d04f3ee4d1e
prabhnith <prabhjot.nith@gmail.com>
2020-05-02 12:21:18 +0530

    Homepage barebone added

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Homepage extends Component{
     render(){
         return (
             <div>
                 <h1> Grillber - Book Your BBQ now!</h1>
+                {/* <Link to="/signin">Signin</Link>
+                <Link to="/signup">Signup</Link> */}
             </div>
         )
     }
