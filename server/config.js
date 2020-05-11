ed4327d1fe192f0b3f102040fb7a1abf71971ed9
prabhnith <prabhjot.nith@gmail.com>
2020-05-11 16:56:41 +0530

    updating mongoDB connection string

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 const config = {
   DB_CONNECTION:
-    "mongodb+srv://pooja:pooja123@grills-kglex.mongodb.net/grill?retryWrites=true&w=majority",
+    // "mongodb+srv://pooja:pooja123@grills-kglex.mongodb.net/grill?retryWrites=true&w=majority",
+    "mongodb+srv://prabhjot:prabh321@grillber-nxovg.mongodb.net/grill?retryWrites=true&w=majority",
 };
 
-module.exports =  config;
+module.exports = config;
